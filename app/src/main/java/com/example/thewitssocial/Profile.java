a68e08fcac7cb5da23ce66758839f5920fc17f2f
ThatBoyAsh <101677234+ThatBoyAsh@users.noreply.github.com>
2022-10-05 22:36:18 +0200

    Add files via upload

--- app/src/main/java/com/example/thewitssocial/Profile.java
+++ app/src/main/java/com/example/thewitssocial/Profile.java
@@ -1,17 +1,25 @@
 package com.example.thewitssocial;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -19,17 +27,34 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+
+import java.util.HashMap;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class Profile extends AppCompatActivity {
 
+    private static final int REQUEST_CODE = 101;
     private FirebaseUser user;
     private DatabaseReference reference;
+    String profileImageUrlv;
+    ImageView profileImageView;
+
+    FirebaseAuth mAuth;
+
 
     private  String userID;
 
     String Name1,Surname1, Email1, PhoneNo1;
 
-    private Button logout;
+
+    private Button logout, friends_btn;
+
+
 
     TextView NameTextView, SurnameTextView, EmailTextView, PhoneNoTextView;
 
@@ -37,6 +62,7 @@ public class Profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
 
         //requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
@@ -46,6 +72,9 @@ public class Profile extends AppCompatActivity {
         user = FirebaseAuth.getInstance().getCurrentUser();
         reference = FirebaseDatabase.getInstance().getReference("Users");
         userID = user.getUid();
+
+        profileImageView = findViewById(R.id.profile_image);
+
 
             NameTextView = (TextView) findViewById(R.id.textView2);
             SurnameTextView = (TextView) findViewById(R.id.textView3);
@@ -77,6 +106,36 @@ public class Profile extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(user ==null){
+            SendUserToLoginActivity();
+        }
+        else{
+            reference.child(userID).addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    if(snapshot.exists()){
+                        profileImageUrlv = snapshot.child("profileImage").getValue().toString();
+                        Picasso.get().load(profileImageUrlv).into(profileImageView);
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                    Toast.makeText(Profile.this,"Something went wrong", Toast.LENGTH_SHORT).show();
+
+                }
+            });
+        }
+    }
+
+    private void SendUserToLoginActivity() {
+        Intent intent = new Intent(Profile.this, MainActivity.class);
+        startActivity(intent);
+    }
+
     public void editProfile(View view) {
 
         Intent intent = new Intent(this, editProfile.class);
@@ -90,4 +149,14 @@ public class Profile extends AppCompatActivity {
     public void test(View view) {
         Toast.makeText(getApplicationContext(),Email1, Toast.LENGTH_SHORT).show();
     }
+
+    public void reset(View view) {
+        Intent intent = new Intent(this,ForgotPassword.class);
+        startActivity(intent);
+    }
+
+ /*   public void openFriends(){
+        Intent intent = new Intent(this,Findfriends.class);
+        startActivity(intent);
+    } */
 }
--- app/src/main/java/com/example/thewitssocial/editProfile.java
+++ app/src/main/java/com/example/thewitssocial/editProfile.java
@@ -1,19 +1,40 @@
 package com.example.thewitssocial;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.HashMap;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class editProfile extends AppCompatActivity {
 
+    private static final int REQUEST_CODE = 101;
+    private  String userID;
+    private FirebaseUser user;
 
 
 
@@ -22,6 +43,15 @@ public class editProfile extends AppCompatActivity {
     String DbsurName;
     String DbeMail;
     String DbpHone;
+
+    //profile pic uploading
+    StorageReference storageRef;
+    FirebaseAuth mAuth;
+    ProgressDialog mloadingBar;
+
+    CircleImageView profileImageView;
+    Uri ImageUri;
+    Button btnSave;
 
 
     DatabaseReference reference;
@@ -34,8 +64,33 @@ public class editProfile extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
 
-        reference = FirebaseDatabase.getInstance().getReference("Users")
-                                             ;//.child(FirebaseAuth.getInstance().getCurrentUser().getUid());
+        profileImageView = findViewById(R.id.profile_image);
+        btnSave = findViewById(R.id.btnSave);
+        mloadingBar = new ProgressDialog(this);
+
+        profileImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(intent,REQUEST_CODE);
+            }
+        });
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SaveData();
+            }
+        });
+
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference("Users");
+        userID = user.getUid();
+        storageRef = FirebaseStorage.getInstance().getReference().child("ProfileImages");
+
+     //   reference = FirebaseDatabase.getInstance().getReference("Users");//.child(FirebaseAuth.getInstance().getCurrentUser().getUid());
 
 
         String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
@@ -74,6 +129,57 @@ public class editProfile extends AppCompatActivity {
      //   Etphone = phoneET.getText().toString();
 
 
+    }
+
+
+    private void SaveData() {
+        if(ImageUri==null){
+            Toast.makeText(this, "select an image", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            mloadingBar.setTitle("adding profile picture");
+            mloadingBar.setCanceledOnTouchOutside(false);
+            mloadingBar.show();
+            storageRef.child(userID).putFile(ImageUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                    if(task.isSuccessful()){
+                        storageRef.child(userID).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                            @Override
+                            public void onSuccess(Uri uri) {
+                                HashMap hashMap = new HashMap();
+                                hashMap.put("profileImage",uri.toString());
+
+                                reference.child(userID).updateChildren(hashMap).addOnSuccessListener(new OnSuccessListener() {
+                                    @Override
+                                    public void onSuccess(Object o) {
+                                        //       Intent intent = new Intent()
+                                        mloadingBar.dismiss();
+                                        Toast.makeText(editProfile.this, "setup complete", Toast.LENGTH_SHORT).show();
+                                    }
+                                }).addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        mloadingBar.dismiss();
+                                        Toast.makeText(editProfile.this, e.toString(),Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                            }
+                        });
+                    }
+                }
+            });
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == REQUEST_CODE && resultCode == RESULT_OK && data!=null){
+            ImageUri = data.getData();
+            profileImageView.setImageURI(ImageUri);
+        }
     }
 
     public void update(View view) {
