fdb45af5e9c8741c0c1d660aea41b17de1bf65ad
Rampamba Lingedzani <102157126+RampLingedzani@users.noreply.github.com>
2022-10-14 18:41:29 +0200

    Update User.java

--- app/src/main/java/com/example/thewitssocial/User.java
+++ app/src/main/java/com/example/thewitssocial/User.java
@@ -1,5 +1,7 @@
 package com.example.thewitssocial;
 
+import com.google.firebase.database.PropertyName;
+
 public class User {
 
     public String Name,Surname,Email,PhoneNo,Password;
@@ -13,5 +15,38 @@ public class User {
         this.Email =Email;
         this.Password = Password;
         this.PhoneNo = PhoneNo;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public void setSurname(String surname) {
+        Surname = surname;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public String getPhoneNo() {
+        return PhoneNo;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        PhoneNo = phoneNo;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    @PropertyName("Name")
+    public String getName() {
+        return Name;
+    }
+    @PropertyName("Surname")
+    public String getSurname() {
+        return Surname;
     }
 }
